ce0dbcb29c73bfee6977530d534561fab5a0d732
mbebe <drazah@o2.pl>
2018-06-29 12:19:43 +0200

    Update waaw.py

--- lib/urlresolver/plugins/waaw.py
+++ lib/urlresolver/plugins/waaw.py
@@ -108,4 +108,3 @@ class WaawResolver(UrlResolver):
 
     def get_url(self, host, media_id):
         return self._default_get_url(host, media_id, template='http://hqq.watch/player/embed_player.php?vid={media_id}&autoplay=no')
-		
